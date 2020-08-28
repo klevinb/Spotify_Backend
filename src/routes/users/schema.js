4d65786ad14ee8df1555e8b18b2d4f97d1ed053a
alekszone <50597224+alekszone@users.noreply.github.com>
2020-08-28 12:09:13 +0200

    working

--- src/routes/users/schema.js
+++ src/routes/users/schema.js
@@ -1,5 +1,6 @@
 const { Schema } = require("mongoose");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
 
 const UserSchema = new Schema({
   name: {
@@ -30,6 +31,29 @@ const UserSchema = new Schema({
     },
   ],
 });
+//check the login
+UserSchema.static.findByCredentials = async(username,password)=>{
+  const user = await UserModel.findOne({username})
+  const ifMatch = await bcrypt.compare(password, user.password)
+  if(!ifMatch){
+    const badLogin = new Error("Your Login Details Are Wrong")
+    badLogin.httpStatusCode = 401
+throw badLogin
+  }
+  return user 
+}
+UserSchema.pre("save", async function(next){
+const data = this
+if(data.isModified("password")){
+  data.password =await bcrypt.hash(data.password, 10)
+}
+next()
+})
+
+
+
+
+
 
 const UserModel = mongoose.model("User", UserSchema);
 
