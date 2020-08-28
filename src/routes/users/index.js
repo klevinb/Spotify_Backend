e7ed3b1a79b919c4c880a837da5e85d3c090fd49
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-08-28 12:13:13 +0200

    UsersRoutes

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -3,4 +3,67 @@ const UserModel = require("./schema");
 
 const router = express.Router();
 
+//Get one:
+router.get('/', async(req, res, next)=>{
+
+    const users = UserModel.find({}, 'username');
+    res.status(200).send(users);
+})
+
+
+//Post a new User:
+router.post('/register', async(req, res, next) =>{
+    const {name, surname, username, password} = req.body;
+
+    const createdUser = new UserModel ({
+        name,
+        surname,
+        username,
+        password
+    });
+
+    await createdUser.save();
+
+    res.status(201).send({createdUser})
+})
+
+// Update:
+router.put('/me', authorize, async(req, res, next) =>{
+    const updates = Object.keys(req.body);
+
+    updates.forEach((update) => (req.user[update] = req.body[update]))
+    await req.user.save();
+    res.status(200).send(req.user);
+})
+
+//Delete:
+router.delete("/me", authorize, async(req, res, next) =>{
+    await req.user.remove();
+    res.status(200).send("User Deleted!");
+})
+
+//Login:
+router.post('/login', async(req, res, next) =>{
+    const {username, password} = req.body;
+    const user = await UserModel.findByCredentials(username, password);
+    const {token, refreshToken} = await AuthenticatorResponse(user);
+    
+    res.send('Logged in!');
+})
+
+//Logout:
+router.post('/logout', authorize, async(req, res, next) =>{
+    req.user.refreshToken = req.user.refreshToken.filter((t) => t.token !== req.body.refreshToken)
+    await req.user.save();
+    res.send('Logged out!');
+ })
+
+ //Log out from all devices:
+router.post('/logoutAll', authorize, async(req, res, next) => {
+    req.user.refreshToken = [];
+    await req.user.save();
+    res.send("Logged out from all devices!")
+})
+
+
 module.exports = router;
