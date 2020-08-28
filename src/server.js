8d4b804f82988543cc67e9dfb4ac9ef17b871346
Klevin Bazaiti <klevin.bazaiti98@gmail.com>
2020-08-28 11:07:37 +0200

    full folders

--- src/server.js
+++ src/server.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const port = process.env.PORT;
+const UserRoutes = require("./routes/users/index");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
-server.listen(port, () => {
-  console.log(`Server runinng port: { ${port} }`);
-});
+server.use("/users", UserRoutes);
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dn7fa.mongodb.net/${process.env.DB_NAME}`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(
+    server.listen(port, () => {
+      console.log(`Server runinng port: { ${port} }`);
+    })
+  )
+  .catch((err) => console.log(err));
